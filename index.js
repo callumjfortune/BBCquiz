36e5b34997bab3bc6768e03537a24c2baf615ce3
callumjfortune <callum.fortune@bbc.co.uk>
2022-11-08 15:16:19 +0000

    index didn't push

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import * as common from "./common.js";
-<<<<<<< HEAD
 window.common = common;
 
-=======
 import * as api from "./api.js";
 
 window.addEventListener("load", function() {
     api.getQuizQuestions("easy", "history").then(console.log);
 });
->>>>>>> e5bcc8879d00a4de0db8c60f2daf09ae044f474e
