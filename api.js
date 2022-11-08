e5bcc8879d00a4de0db8c60f2daf09ae044f474e
James Livesey <james.livesey@bbc.co.uk>
2022-11-08 14:57:22 +0000

    Add JS module for getting quiz data The file `api.js` will contain a bunch of useful functions which we will use to query the `quiz_data.json` file. We can also use it for handling incoming analytics data in the future.
    Right now, we have one function called `getQuizQuestions` which returns
    a `Promise` that resolves to an array of `QuizQuestion` class instances.
    We can use subclasses of the `QuizQuestion` class to implement things
    such as validation, which will help in the long run.

--- api.js
+++ api.js
@@ -2,8 +2,29 @@ import * as common from "./common.js";
 
 var quizData = null;
 
-export class QuizItem {
-    constructor() {}
+export class QuizQuestion {
+    constructor(question) {
+        this.question = question;
+    }
+
+    static deserailise(data) {
+        switch ("multiple_choice") { // Just hard-coded to multiple choice type for now
+            case "multiple_choice":
+                return new MultipleChoiceQuizQuestion(data.question, data.options, data.answer);
+
+            default:
+                throw new Error("Invalid question type");
+        }
+    }
+}
+
+export class MultipleChoiceQuizQuestion extends QuizQuestion {
+    constructor(question, options, answer) {
+        super(question);
+
+        this.options = options;
+        this.answer = answer;
+    }
 }
 
 function getQuizData() {
@@ -11,7 +32,7 @@ function getQuizData() {
         return Promise.resolve(quizData);
     }
 
-    fetch("/quiz_data.json").then(function(response) {
+    return fetch("/quiz_data.json").then(function(response) {
         return response.json();
     }).then(function(data) {
         quizData = data;
@@ -20,14 +41,22 @@ function getQuizData() {
     });
 }
 
-export function getQuizItems(difficulty, topic) {
+export function getQuizQuestions(difficulty, topic) {
     return getQuizData().then(function() {
         var difficultyData = quizData.difficulties[difficulty];
 
         if (!difficultyData) {
-            return Promise.reject("Difficulty does not exist");
+            return Promise.reject(`Difficulty "${difficulty}" does not exist`);
         }
 
-        
+        var topicData = difficultyData.topics[topic];
+
+        if (!topicData) {
+            return Promise.reject(`Topic "${topic}" for given difficulty "${difficulty}" does not exist`);
+        }
+
+        return Promise.resolve(topicData.questions.map(function(questionData) {
+            return QuizQuestion.deserailise(questionData);
+        }));
     });
 }
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 import * as common from "./common.js";
+import * as api from "./api.js";
+
+window.addEventListener("load", function() {
+    api.getQuizQuestions("easy", "history").then(console.log);
+});
