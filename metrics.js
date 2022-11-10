6e35d77de4c14bd97559f2ef84bdc557e1480b93
James Livesey <james.livesey@bbc.co.uk>
2022-11-10 14:22:02 +0000

    Add data viewing table on analytics page

--- metrics.js
+++ metrics.js
@@ -3,11 +3,34 @@ import * as common from "./common.js";
 var analyticsData = {};
 var analyticsEvent = "loadQuiz";
 
+function renderAnalyticsViewer() {
+    var eventData = analyticsData[analyticsEvent];
+
+    $("#analyticsViewer").empty();
+
+    $("#analyticsViewer").append([
+        $("<thead>").append(
+            $("<tr>").append(
+                ...Object.keys(eventData[eventData.length - 1]).map((field) => $("<th>").text(field))
+            )
+        ),
+        $("<tbody>").append(
+            eventData.map((datum) => $("<tr>").append(
+                Object.values(datum).map((value) => $("<td>").text(value))
+            ))
+        )
+    ]);
+}
+
 $(function() {
     analyticsData = common.getStorageData("analytics");
 
+    renderAnalyticsViewer();
+
     $("#analyticsEvent").on("change", function() {
         analyticsEvent = $("#analyticsEvent").val();
+
+        renderAnalyticsViewer();
     });
 
     $("#exportCsv").on("click", function() {
